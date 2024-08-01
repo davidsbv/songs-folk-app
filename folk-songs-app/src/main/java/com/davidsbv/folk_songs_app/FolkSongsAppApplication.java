e7cfc59a811dfff3ea9208d40b34e58eda9c612b
davidsbv <163351521+davidsbv@users.noreply.github.com>
2024-08-01 20:34:17 +0200

    Add files via upload

---
+++ folk-songs-app/src/main/java/com/davidsbv/folk_songs_app/FolkSongsAppApplication.java
@@ -0,0 +1,13 @@
+package com.davidsbv.folk_songs_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FolkSongsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FolkSongsAppApplication.class, args);
+	}
+
+}
---
+++ folk-songs-app/src/test/java/com/davidsbv/folk_songs_app/FolkSongsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davidsbv.folk_songs_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FolkSongsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
